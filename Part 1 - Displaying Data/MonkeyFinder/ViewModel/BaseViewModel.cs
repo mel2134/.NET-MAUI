8f5bdf5a0b95058f81ca2887ffe9f7f5067d8086
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-05 14:48:11 +0300

    view model again.

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,34 +1,42 @@
 ﻿namespace MonkeyFinder.ViewModel;
-
-public class BaseViewModel : INotifyPropertyChanged
+//[INotifyPropertyChanged]
+public partial class BaseViewModel:ObservableObject/* : INotifyPropertyChanged*/
 {
+    public BaseViewModel()
+    {
+        //SetProperty(ref isBusy,true);
+    }
+    [ObservableProperty]
+   // [AlsoNotifyChangeFor(nameof(IsNotBusy))]
     bool isBusy;
+    [ObservableProperty]
     string title;
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value) return;
-            isBusy = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
-    }
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value) return;
-            title = value;
-            OnPropertyChanged();
-        }
-    }
     public bool IsNotBusy => !IsBusy;
-    public event PropertyChangedEventHandler PropertyChanged;
-    public void OnPropertyChanged([CallerMemberName]string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
+    //public bool IsBusy
+    //{
+    //    get => isBusy;
+    //    set
+    //    {
+    //        if (isBusy == value) return;
+    //        isBusy = value;
+    //        OnPropertyChanged();
+    //        OnPropertyChanged(nameof(IsNotBusy));
+    //    }
+    //}
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title == value) return;
+    //        title = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+    //public bool IsNotBusy => !IsBusy;
+    //public event PropertyChangedEventHandler PropertyChanged;
+    //public void OnPropertyChanged([CallerMemberName]string name = null)
+    //{
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //}
 }
