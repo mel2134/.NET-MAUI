382d6481c51d00d2cd775352eea8463c63c5da8f
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-05 14:41:37 +0300

    baseviewmodel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,34 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public class BaseViewModel : INotifyPropertyChanged
 {
+    bool isBusy;
+    string title;
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value) return;
+            isBusy = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value) return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+    public bool IsNotBusy => !IsBusy;
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName]string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
