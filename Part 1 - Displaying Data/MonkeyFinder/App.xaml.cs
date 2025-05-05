17276925fa70af176083929480d96ae0ee732613
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-05 14:06:22 +0300

    Monkey model

--- Part 1 - Displaying Data/MonkeyFinder/App.xaml.cs
+++ Part 1 - Displaying Data/MonkeyFinder/App.xaml.cs
@@ -1,13 +1,31 @@
 ﻿namespace MonkeyFinder;
-
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 public partial class App : Application
 {
 	public App()
 	{
-		InitializeComponent();
-	}
+        const int WindowWidth = 540;
+        const int WindowHeight = 1000;
+        InitializeComponent();
+#if WINDOWS
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId Id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(Id);
+                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+            });
+#endif
+    }
 
-	protected override Window CreateWindow(IActivationState activationState)
+    protected override Window CreateWindow(IActivationState activationState)
 	{
 		return new Window(new AppShell());
 	}
