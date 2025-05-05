17cab60d3b94fcfbdd0d3a07e8e68a19c6550605
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-05 16:16:48 +0300

    Geolocation that doesnt work on windows

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -6,11 +6,40 @@ public partial class MonkeysViewModel : BaseViewModel
     MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
     //public Command GetMonkeysCommand { get; }
-    public MonkeysViewModel(MonkeyService monkeyService)
+    IConnectivity connectivity;
+    IGeolocation geolocation;
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
+
         //GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
+
+    }
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0) return;
+        try
+        {
+            var loc = await geolocation.GetLastKnownLocationAsync();
+            if (loc is null) loc = await geolocation.GetLocationAsync(new GeolocationRequest{ DesiredAccuracy= GeolocationAccuracy.Medium,Timeout=TimeSpan.FromSeconds(30)});
+            
+            if (loc is null) return;
+
+            var first = Monkeys.OrderBy(m => loc.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)).FirstOrDefault();
+            
+            if (first is null) return;
+
+            await Shell.Current.DisplayAlert("Closest monkey",$"{first.Name} in {first.Location}","OK!");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get closest monkey: {ex}", "OK");
+        }
     }
     [RelayCommand]
     async Task GoToDetailsAsync(Monkey monkey)
@@ -28,6 +57,11 @@ public partial class MonkeysViewModel : BaseViewModel
         if (IsBusy) return;
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("No Internet", $"No internet!", "OK");
+                return;
+            }
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
             if (Monkeys.Count != 0) Monkeys.Clear();
