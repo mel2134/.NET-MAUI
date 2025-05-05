aa79f51056416bfb22f055068e5aa66ad7646ffd
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-05-05 14:56:16 +0300

    monkey service

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,29 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0) return monkeyList;
+        string url = "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json";
+        var resp = await httpClient.GetAsync(url);
+
+        if (resp.IsSuccessStatusCode)
+        {
+            monkeyList = await resp.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+        //using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        //using var reader = new StreamReader(stream);
+        //var contents = await reader.ReadToEndAsync();
+        //monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);
+        return monkeyList;
+    }
 }
